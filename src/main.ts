34520099ff13b14475e6040db358af12c662e10e
Simon Marenkov <simon.marenkov@gmail.com>
2024-09-29 17:27:40 +0300

    Testing docker

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
